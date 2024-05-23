326f242f6d67219a5cfed547ceb28967f1f01494
Carlos Andrés Castilla García <carloscastilla31@gmail.com>
2024-05-23 12:49:27 -0500

    add files

---
+++ app/infrastructure/data/data_frame_manager.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from typing import Type, List, TypeVar
+
+from app.domain.contracts.Idata_frame_manager import IDataFrameManager
+from app.domain.contracts.Idata_loader import IDataLoader
+from app.domain.base import BaseModel
+
+T = TypeVar('T', bound=BaseModel)
+
+
+class DataFrameManager(IDataFrameManager):
+    def __init__(self, data_loader: IDataLoader, data_directory: str):
+        self.data_loader = data_loader
+        self.data_directory = data_directory
+        self.dataframe = self.data_loader.load_parquet_files(data_directory)
+
+    def query(self, model: Type[T]) -> List[T]:
+        return model.from_dataframe(self.dataframe)
