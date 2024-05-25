b535f5205b3d9a4eaa7c3fb521e3ad75339e91d8
carlos andres castilla garcia <carloscastilla31@gmail.com>
2024-05-24 23:35:07 -0500

    Add file handler for logging to create and write logs to app.log in the root directory

--- app/infrastructure/logging_config.py
+++ app/infrastructure/logging_config.py
@@ -1,8 +1,24 @@
 import logging
 
+import logging
+import os
+
 
 def setup_logging():
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    log_file = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), '../../', 'app.log'))
+
+    # Crear un directorio para los logs si no existe
+    log_dir = os.path.dirname(log_file)
+    if log_dir and not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        handlers=[
+                            logging.FileHandler(log_file),
+                            logging.StreamHandler()
+                        ])
     return logging.getLogger(__name__)
 
 
